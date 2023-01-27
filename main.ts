e16a362dbdf463e1358c05a4de4ee2ad7aff34c0
Alberto2807 <123153884+Alberto2807@users.noreply.github.com>
2023-01-27 09:25:38 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-basic.forever(function () {
-	
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("" + (input.temperature()))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showString("" + (input.lightLevel()))
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (input.soundLevel()))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("B")
 })
